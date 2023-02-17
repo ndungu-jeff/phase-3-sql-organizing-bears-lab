220bbd30db2eccb60e5abe018c5a39b38d6f0e36
jeff ndungu <ndungujeff.ke@gmail.com>
2023-02-17 12:22:47 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1,'Mr. Chocolate', 7, 'M','Dark','Sangine',1),
+       (2, 'Rowdy', 9,'F', 'Pale','Choleric',1),
+       (3, 'Tabitha', 11,'F', 'Blonde','Sanguine',1),
+       (4, 'Sergeant Brown', 13,'M', 'Melanin','Supine',1),
+       (5, 'Melissa', 15,'F', 'Brown','Phlegmstic',0),
+       (6, 'Grinch', 17,'M', 'Olive','Melancholy',1),
+       (7, 'Wendy', 19,'F', 'Dark Brown','Phlegmstic',0),
+       (8, NULL,NULL, 'M', 'Dark Brown','Phlegmstic',1) 
+       
